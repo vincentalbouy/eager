1cbf49e538a7e53a792d64a48b8142b3b2aad9a9
Vincent Albouy <vincentalbouy@vincents-mbp.almaden.ibm.com>
2017-06-28 13:35:47 -0700

    bag of words based on Kaggle tutorial

---
+++ BagOfwords/BOW.py
@@ -0,0 +1,7 @@
+# Import the pandas package, then use the "read_csv" function to read
+# the labeled training data
+import pandas as pd
+
+
+#train = pd.read_csv("labeledTrainData.tsv", header=0, \
+ #                   delimiter="\t", quoting=3)
